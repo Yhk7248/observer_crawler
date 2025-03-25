9b0693b6c924a176ef6e085b83ae8218c4ce522e
joseph <younghyun7248@gmail.com>
2025-03-26 00:40:26 +0900

    krx 스케줄러 수정

--- scheduler/krx_scheduler.py
+++ scheduler/krx_scheduler.py
@@ -23,7 +23,7 @@ def job():
     start_crawler('price')
 
 
-schedule.every().day.at("22:30").do(job)  # 매일 09:00
+schedule.every().day.at("22:30").do(job)
 
 while True:
     schedule.run_pending()
